6eb90999fe0cfa435818c6b55671e80ebc2ddd0f
carina hoerst <carina.h1408@googlemail.com>
2021-03-31 20:40:09 +0100

    corrections

--- r_docs/corrections.R
+++ r_docs/corrections.R
@@ -11,11 +11,11 @@ US_t1 <- US_t1 %>%
 US_t1 <- US_t1 %>%
   mutate(vote = as.numeric(vote))
 
-# individual-based: correlation between own approval (among Republicans) and estimated consensus
+# within-group: correlation between own approval (among Republicans) and estimated consensus
 cor.test(US_vote_Rep_t1$fc_1_s, US_vote_Rep_t1$fc_1_o_agr)
+cor.test(US_vote_Dem_t1$fc_1_s, US_vote_Dem_t1$fc_1_o_agr)
 
-
-# group-based: correlation between party support and estimated consensus
+# between-group: correlation between party support and estimated consensus
 cor.test(US_t1$vote, US_t1$fc_1_o_agr)
 
 US_t1 <- US_t1 %>%
@@ -25,14 +25,17 @@ US_t1 <- US_t1 %>%
 
 # plot association between fc_1_s and fc_1_o_agr by vote
 
-ggplot(data=US_t1,aes(x = fc_1_s, y = fc_1_o_agr, color = vote))+
-  geom_point(position = position_dodge(width = 0.5))+
+ggplot(data = US_t1,aes(x=fc_1_s,y=fc_1_o_agr, color = vote))+
+  stat_ellipse(expand = 0,aes(fill=vote))+
+  geom_point(position = "jitter")+
+  scale_y_continuous(name = "Mean estimated agreement fc1") +
+  scale_x_continuous(name = "Mean own approval fc1") +
   scale_color_grey() +
   labs(color = "Party support") +
   cowplot::theme_cowplot()
   
 
-## subsequently: between-group analysis: 
+## subsequently: between-group analysis (because focus of this paper): 
 
 # "Consistent with Ross et al. (1977), false consensus was operationalized as existing when individuals rate the incidence 
 # of their own opinions and behaviors in the wider community more highly than those who do not share those opinions and 
@@ -67,6 +70,83 @@ t.test(US_t1_Rep$fc_1_o_agr, mu = 43.4)
 
 
 ## change in false consensus
+
+# within-groups: conduct t-tests on difference (like what I have already done in paper)
+
+# t-test
+t.test(x, mu = y)
+
+# plot
+US$time <- as.factor(US$time)
+
+plot_summary <- US %>%
+  dplyr::group_by(time, vote) %>%
+  dplyr::summarise(n = n(),
+                   mean_fc_1 = mean(fc_1_s),
+                   sd_approval_fc_1 = sd(fc_1_s),
+                   mean_agree_fc_1 = mean(fc_1_o_agr), 
+                   sd_agree_fc_1 = sd(fc_1_o_agr),
+                   se_agree_fc_1 = sd_agree_fc_1/sqrt(n),
+                   lower_ci_agree_fc_1 = (mean_agree_fc_1 - (1.96*se_agree_fc_1)),
+                   upper_ci_agree_fc_1 = (mean_agree_fc_1 + (1.96*se_agree_fc_1)),
+                   mean_disagr_fc_1 = mean(fc_1_o_disagr),
+                   sd_disagr_fc_1 = sd(fc_1_o_disagr),
+                   mean_fc_2 = mean(fc_2_s),
+                   sd_approval_fc_2 = sd(fc_2_s),
+                   mean_agree_fc_2 = mean(fc_2_o_agr), 
+                   sd_agree_fc_2 = sd(fc_2_o_agr),
+                   se_agree_fc_2 = sd_agree_fc_2/sqrt(n),
+                   lower_ci_agree_fc_2 = (mean_agree_fc_2 - (1.96*se_agree_fc_2)),
+                   upper_ci_agree_fc_2 = (mean_agree_fc_2 + (1.96*se_agree_fc_2)),
+                   mean_disagr_fc_2 = mean(fc_2_o_disagr),
+                   sd_disagr_fc_2 = sd(fc_2_o_disagr))
+plot_summary
+
+
+# plot 1
+
+US <- tibble(
+  vote = c('Democrats', 'Republicans', 'Democrats', 'Republicans'),
+  time = c('Pre', 'Pre', 'Post', 'Post'),
+  val = c(43.4, 61.6, 45.3, 61.2),
+)
+
+n_groups <- length(unique(US$vote))
+group_names <- unique(US$vote)
+
+US %>%
+  mutate(vote = factor(vote)) %>%
+  # Manually calculate x positions
+  mutate(x = as.integer(vote) - (n_groups + 1) * (time == "Pre")) %>%
+  ggplot(aes(x = x, y = val, vote = vote, col = vote, shape = vote)) +
+  geom_point(shape = 16, size = 2, pch = 23) +
+  geom_line(linetype = 3) +
+  # Manually add labels
+  scale_x_continuous(breaks = (-n_groups):n_groups,
+                     labels = c(group_names, 'Election', group_names))+
+  scale_y_continuous(name = "Consensus estimation (Rep/agreement; Dem/disagreement)")+
+  scale_color_grey() +
+  cowplot::theme_cowplot() + 
+  geom_vline(xintercept = 0, linetype = "dashed")
+
+
+
+# plot 2
+plot_summary %>%
+  ggplot(aes(x = time, y = mean_agree_fc_1, group = vote)) +
+  geom_point(aes(color = vote), shape = 16, size = 2, pch = 23, position = "dodge") +
+  geom_errorbar(aes(ymin = lower_ci_agree_fc_1, ymax = upper_ci_agree_fc_1, color = vote),position = "dodge",  width = .05) +
+  scale_color_grey() +
+  geom_line(linetype = 3) + 
+  scale_x_discrete(name = "Time point") +
+  scale_y_continuous(name = "Mean agreement estimation FC1 (%)") +
+  labs(color = "Party support") +
+  cowplot::theme_cowplot()
+
+
+
+# between-groups: # t1 Rep/agree - Dem/disagree (= value one in t-test)  vs t2 Rep/agree - Dem/disagree (= reference value in t-test)
+
 # compare tables by time point
 US_t1 %>% 
   dplyr::group_by(vote) %>% 
@@ -98,47 +178,69 @@ US_t2 %>%
                    mean_disagr_fc_2 = mean(fc_2_o_disagr),
                    sd_disagr_fc_2 = sd(fc_2_o_disagr))
 
-# plot?
-US$time <- as.factor(US$time)
-
-plot_summary <- US %>% 
-  dplyr::mutate(time = case_when(
-    time =="1" ~ "-.5",
-    time =="2" ~ ".5")) %>%
-  dplyr::mutate(time, levels = c("Pre", "Post")) %>% # this command doesnt work!!!
-  dplyr::group_by(time, vote) %>%
-  dplyr::summarise(n = n(),
-                   mean_fc_1 = mean(fc_1_s),
-                   sd_approval_fc_1 = sd(fc_1_s),
-                   mean_agree_fc_1 = mean(fc_1_o_agr), 
-                   sd_agree_fc_1 = sd(fc_1_o_agr),
-                   se_agree_fc_1 = sd_agree_fc_1/sqrt(n),
-                   lower_ci_agree_fc_1 = (mean_agree_fc_1 - (1.96*se_agree_fc_1)),
-                   upper_ci_agree_fc_1 = (mean_agree_fc_1 + (1.96*se_agree_fc_1)),
-                   mean_disagr_fc_1 = mean(fc_1_o_disagr),
-                   sd_disagr_fc_1 = sd(fc_1_o_disagr),
-                   mean_fc_2 = mean(fc_2_s),
-                   sd_approval_fc_2 = sd(fc_2_s),
-                   mean_agree_fc_2 = mean(fc_2_o_agr), 
-                   sd_agree_fc_2 = sd(fc_2_o_agr),
-                   se_agree_fc_2 = sd_agree_fc_2/sqrt(n),
-                   lower_ci_agree_fc_2 = (mean_agree_fc_2 - (1.96*se_agree_fc_2)),
-                   upper_ci_agree_fc_2 = (mean_agree_fc_2 + (1.96*se_agree_fc_2)),
-                   mean_disagr_fc_2 = mean(fc_2_o_disagr),
-                   sd_disagr_fc_2 = sd(fc_2_o_disagr))
-plot_summary
-
-ggplot(data = plot_summary, aes(x = time, y = mean_agree_fc_2, group = vote)) +
-  geom_point(aes(color = vote), shape = 16, size = 2, pch = 23, position = "dodge") +
-  geom_errorbar(aes(ymin = lower_ci_agree_fc_2, ymax = upper_ci_agree_fc_2, color = vote),position = "dodge",  width = .05) +
-  scale_color_grey() +
-  geom_line(linetype = 3) + 
-  scale_x_discrete(name = "Time point") +
-  scale_y_continuous(name = "Mean agreement estimation FC2 (%)") +
-  labs(color = "Party support") +
-  cowplot::theme_cowplot()
+# t-test
+t.test(x, mu = y)
 
 
-## using false consensus measure as predictor
 
-# TFCE as score that is used as predictor
+## using false consensus measure as predictor (Bauman & Geher (2002))
+
+# Calculating false consensus scores. Initially, participants' false consensus scores were calculated in a number of 
+# different ways. Based on Krueger and Zeiger's (1993) truly false consensus measure, a score was computed for each 
+# individual which assessed his/her overall tendency (across all issues) to overestimate support for his/her position. A
+# similar measure was computed based on people's behavioral endorsements and estimates, rather than their attitudes. 
+# Ultimately, neither of these indices of false consensus was significantly related to behavioral intentions for the issues 
+# incorporated in this research. Thus, false consensus scores were computed for each issue by subtracting the actual 
+# consensus (based on the actual percentage of people who agreed with a particular issue) from each person's estimate to 
+# determine the extent to which he or she overestimated support for his or her position using the behavioral measure. 
+
+# actual consensus = frequency of endorsement of items
+# estimated consensus = estimated agreement 
+
+# TFCE = estimated agreement - actual consensus
+
+# establish actual consensus time 1
+
+approval_fc_1_s_t1 <- US_t1 %>% 
+  dplyr::filter(fc_1_s >= 4) %>%
+  nrow()
+approval_fc_1_s_t1 # results in 41 (out of 139 --> 29.5%)
+
+# establish actual consensus time 2
+approval_fc_1_s_t2 <- US_t2 %>% 
+  dplyr::filter(fc_1_s >= 4) %>%
+  nrow()
+approval_fc_1_s_t2 # results in 37 (out of 139 --> 26.6%)
+
+# --> establish overall mean of estimated agreement for Republicans
+
+US_Rep <- subset(US, vote == "Republicans")
+
+# old
+US_Rep <- US_Rep %>% 
+  group_by(id) %>%
+  dplyr::mutate(fc_1_o_agr_sum = sum(fc_1_o_agr),
+                mean_fc_1_o_agr = fc_1_o_agr_sum/2, 
+                TFCE = mean_fc_1_o_agr -61) # minus 61 from the overall mean 
+
+# minus the actual consensus from estimated per time each
+US_Rep <- US_Rep %>%
+  dplyr::mutate(TFCE_t1 = fc_1_o_agr - 29.5,
+                TFCE_t2 = fc_1_o_agr - 26.6) 
+
+# export file and deleted every row that was wrongly calculated by hand!
+write.csv(US_Rep, "US_Rep.csv")
+
+US_Rep <- read_csv("./data/US_Rep.csv")
+
+# "endorsement" ?? see Krueger & Zeiger (1993)
+
+
+
+
+
+
+
+
+
+
